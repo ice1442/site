407e5451851202496e1e9c609043104423cfd6fa
Lukas <63304771+Lukas-14@users.noreply.github.com>
2021-04-20 17:58:51 +0200

    Add files via upload

--- web_database.js
+++ web_database.js
@@ -79,13 +79,13 @@ function getscorelist(){
     start = false;
     x = 0;
     sorta = alist.sort(function(a,b){
-        return a.score - b.score;
+        return b.score - a.score;
     });
     sortb = blist.sort(function(a,b){
-        return a.score - b.score;
+        return b.score - a.score;
     });
     sortc = clist.sort(function(a,b){
-        return a.score - b.score;
+        return b.score - a.score;
     });
     console.log(sorta);
     console.log(sortb);
