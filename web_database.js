4f8a157167eb23216c68cea07ecc503e5f430a48
Lukas <63304771+Lukas-14@users.noreply.github.com>
2021-04-21 09:34:50 +0200

    Add files via upload

--- web_database.js
+++ web_database.js
@@ -70,6 +70,7 @@ refa.orderByChild("score").on("child_added", function(data) {
     alist.push(helloa);
 });
 let sorta = alist.sort(function(a,b){
+    console.log(sorta);
     return b.score - a.score;
 });
 var refb = firebase.database().ref('records/team B');
